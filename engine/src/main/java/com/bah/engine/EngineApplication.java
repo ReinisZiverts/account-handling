7f2a839c0a4cb7ef3a4065b3b1d4e3126c3e88fe
Reinis <ziverts.reinis@gmail.com>
2026-06-19 21:48:07 +0300

    Initial commit

---
+++ engine/src/main/java/com/bah/engine/EngineApplication.java
@@ -0,0 +1,15 @@
+package com.bah.engine;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class EngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EngineApplication.class, args);
+    }
+
+}
